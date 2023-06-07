d8e1f1d739fd7b9ded22d6aee6e6780cd882061a
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-06-07 12:53:00 -0400

    Lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe()
+{
+   return(
+    <div>
+        <button 
+        onFocus={()=> console.log("Good!") }
+        onBlur={() => console.log("Hey! Eyes on me!")}
+        >Eyes on me</button>
+    </div>
+   )
+}
+export default EyesOnMe
